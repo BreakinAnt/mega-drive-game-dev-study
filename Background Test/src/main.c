cb655ca33ccb805699f1b8048d8cc0b8f88d597f
Felipe Carneiro <flipecar97@gmail.com>
2023-08-12 01:48:09 -0300

    added bg parallaxing

--- Background Test/src/main.c
+++ Background Test/src/main.c
@@ -2,15 +2,18 @@
 #include <genesis.h>
 #include <resources.h>
 
+u16 ind = TILE_USER_INDEX;
+int hscroll_offset_b = 0;
+int hscroll_offset_a = 0;
+
 int main()
 {
-    u16 ind = TILE_USER_INDEX;
     PAL_setPalette(PAL0, bg1.palette->data, DMA);
     VDP_drawImageEx(
         BG_B, 
         &bg1, 
         TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, ind),
-        0, 0,
+        0, -4,
         FALSE,
         TRUE
     );
@@ -20,15 +23,19 @@ int main()
         BG_A, 
         &fg1, 
         TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, ind),
-        0, 0,
+        0, -4,
         FALSE,
         TRUE
     );
     ind += fg1.tileset->numTile;
-    
+
+    VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
+
     while(1)
     {
-        //For versions prior to SGDK 1.60 use VDP_waitVSync instead.
+        VDP_setHorizontalScroll(BG_B, hscroll_offset_b--);
+        VDP_setHorizontalScroll(BG_A, hscroll_offset_a-=2); 
+    
         SYS_doVBlankProcess();
     }
     return (0);
